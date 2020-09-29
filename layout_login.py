9d559020791580c2511c44eba35def5ff6e1e6d9
KJAYALATHA <71957324+KJAYALATHA@users.noreply.github.com>
2020-09-29 18:02:41 +0530

    Add files via upload

--- layout_login.py
+++ layout_login.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 w=Tk()
 w.geometry("400x300")
 w.title("login")
@@ -9,5 +10,17 @@ username.grid(row=0,column=1)
 Label(text="password").grid(row=1,column=0)
 password=Entry(show="*")
 password.grid(row=1,column=1)
-Button(text="login").grid(row=2,column=0,columnspan=2)
+def login():
+    uname=username.get()
+    pwd=password.get()
+    f=open("admin.txt","r")
+    print(uname)
+    for i in f:
+        print(i.split(" ")[0])
+        if (i.split(" ")[0])==uname and pwd in i.split(" ")[1]:
+            messagebox.showinfo("login","authorized user")
+            break
+    else:
+        messagebox.showinfo("unauthorized user")
+Button(text="login",command=login).grid(row=2,column=0,columnspan=2)
 w.mainloop()
